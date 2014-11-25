c7852de50103419fb32a3a244da67ba8a0eb6762
Laurent Petit <laurent.petit@gmail.com>
2014-11-25 23:56:43 +0100

    initial import of the feature

---
+++ README.md
@@ -0,0 +1,33 @@
+# ccw-plugin-zoom
+
+This Counterclockwise plugin installs keybindings to Eclipse to facilitate zoom/unzoom of editor/repl fonts.
+
+This plugin's state is stable.
+
+## Install
+
+The `~/.ccw/` folder is where Counterclockwise searches for User Plugins.
+
+It is recommended to layout User Plugins inside this folder by mirroring Github's namespacing. So if you clone laurentpetit/ccw-plugin-zoom, you should do the following:
+
+- Create a folder named `~/.ccw/laurentpetit/`
+- Clone this project from `~/.ccw/laurentpetit/`
+
+        mkdir -p ~/.ccw/laurentpetit
+        cd ~/.ccw/laurentpetit
+        git clone https://github.com/laurentpetit/ccw-plugin-zoom.git
+
+- If you have already installed ccw-plugin-manager (https://github.com/laurentpetit/ccw-plugin-manager.git), then type `Alt+U S` to re[S]tart User Plugins (and thus ccw-plugin-zoom will be found and loaded)
+- If you have not already installed ccw-plugin-manager, restart your Eclipse / Counterclockwise/Standalone instance.
+
+## Usage
+
+- Use 'Alt+U Z' for increasing the fonts size (Zooming)
+- Use 'Alt+U U' for decreasing the fonts size (Unzooming)
+
+## License
+
+Copyright © 2009-2015 Laurent Petit
+
+Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ ccw-plugin-zoom.clj
@@ -0,0 +1,66 @@
+(ns ccw-plugin-zoom
+  "This Counterclockwise plugin installs keybindings to Eclipse to facilitate
+   zoom/unzoom of editor/repl fonts.
+   Use 'Alt+U Z' for increasing the fonts size (Zooming)
+   Use 'Alt+U U' for decreasing the fonts size (Unzooming)"
+  (:require
+    [ccw.e4.dsl :refer :all]
+    [ccw.eclipse :as e]
+    [ccw.leiningen.launch :as l]
+    [ccw.e4.model :as m]
+    [clojure.string :as str]))
+
+(def font-increment-step
+  "Select the increment step by which the font size must be increased or decreased"
+  2)
+
+(defn update-font
+  "Fonts are serialized as:
+   1|.Helvetica Neue DeskInterface|12.0|0|COCOA|1|.HelveticaNeueDeskInterface-Regular;
+   Given a serialized font s, then apply function f to the part at index idx
+   (starting at 0).
+   Example: (update-font
+              \"1|.Helvetica Neue DeskInterface|12.0|0|COCOA|1|.HelveticaNeueDeskInterface-Regular;\"
+              2
+              (constantly \"16.0\"))
+            would return \"1|.Helvetica Neue DeskInterface|16.0|0|COCOA|1|.HelveticaNeueDeskInterface-Regular;\""
+  [s idx f]
+  (let [parts (vec (str/split s #"\|"))
+        new-parts (update-in parts [idx] f)]
+    (str/join "|" new-parts)))
+
+(defn update-font-size
+  "Updates the font size by adding increment to it"
+  [s increment]
+  (update s 2
+    (comp #(+ % increment) #(java.lang.Double/parseDouble %))))
+
+(def font-prefs-keys
+  "Eclipse font preferences keys we want to increment/decrement altogether"
+  ["org.eclipse.jface.textfont"
+   "org.eclipse.debug.ui.VariableTextFont"
+   "org.eclipse.jface.dialogfont"
+   "org.eclipse.ui.workbench.TAB_TEXT_FONT"
+   "org.eclipse.ui.workbench.VIEW_MESSAGE_TEXT_FONT"
+   "org.eclipse.ui.workbench.texteditor.blockSelectionModeFont"])
+
+(defn zoomer
+  "Adds increment to font sizes"
+  [increment context]
+  (doseq [pref font-prefs-keys]
+    (let [before (e/preference "org.eclipse.ui.workbench" pref nil)
+          after (update-font-size before increment)]
+      (e/preference! "org.eclipse.ui.workbench" pref after))))
+
+(defn zoom-in-hdl [context] (zoomer font-increment-step context))
+(defn zoom-out-hdl [context] (zoomer (- font-increment-step) context))
+
+(defcommand zoom-in "Zoom: increase the fonts size (editors, repls, etc.)")
+(defhandler zoom-in zoom-in-hdl)
+(defkeybinding zoom-in "Alt+U Z")
+
+(defcommand zoom-out "Unzoom: decrease the fonts size (editors, repls, etc.)")
+(defhandler zoom-out zoom-out-hdl)
+(defkeybinding zoom-out "Alt+U U")
+
+
