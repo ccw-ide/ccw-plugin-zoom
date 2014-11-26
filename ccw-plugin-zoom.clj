4c3e348f916bdef4de14bde4c976716550995253
Laurent Petit <laurent.petit@gmail.com>
2014-11-26 01:58:16 +0100

    fix(fonts): simplified working with fonts and more robust also

--- ccw-plugin-zoom.clj
+++ ccw-plugin-zoom.clj
@@ -12,45 +12,34 @@
 
 (def font-increment-step
   "Select the increment step by which the font size must be increased or decreased"
-  2)
-
-(defn update-font
-  "Fonts are serialized as:
-   1|.Helvetica Neue DeskInterface|12.0|0|COCOA|1|.HelveticaNeueDeskInterface-Regular;
-   Given a serialized font s, then apply function f to the part at index idx
-   (starting at 0).
-   Example: (update-font
-              \"1|.Helvetica Neue DeskInterface|12.0|0|COCOA|1|.HelveticaNeueDeskInterface-Regular;\"
-              2
-              (constantly \"16.0\"))
-            would return \"1|.Helvetica Neue DeskInterface|16.0|0|COCOA|1|.HelveticaNeueDeskInterface-Regular;\""
-  [s idx f]
-  (let [parts (vec (str/split s #"\|"))
-        new-parts (update-in parts [idx] f)]
-    (str/join "|" new-parts)))
-
-(defn update-font-size
-  "Updates the font size by adding increment to it"
-  [s increment]
-  (update-font s 2
-    (comp #(+ % increment) #(java.lang.Double/parseDouble %))))
+  2.0)
 
 (def font-prefs-keys
   "Eclipse font preferences keys we want to increment/decrement altogether"
-  ["org.eclipse.jface.textfont"
+  [
+   "org.eclipse.jface.textfont"
    "org.eclipse.debug.ui.VariableTextFont"
    "org.eclipse.jface.dialogfont"
-   "org.eclipse.ui.workbench.TAB_TEXT_FONT"
-   "org.eclipse.ui.workbench.VIEW_MESSAGE_TEXT_FONT"
-   "org.eclipse.ui.workbench.texteditor.blockSelectionModeFont"])
+   ; "org.eclipse.ui.workbench.TAB_TEXT_FONT"
+   ; "org.eclipse.ui.workbench.VIEW_MESSAGE_TEXT_FONT"
+   "org.eclipse.ui.workbench.texteditor.blockSelectionModeFont"
+  ])
 
 (defn zoomer
   "Adds increment to font sizes"
   [increment context]
-  (doseq [pref font-prefs-keys]
-    (let [before (e/preference "org.eclipse.ui.workbench" pref nil)
-          after (update-font-size before increment)]
-      (e/preference! "org.eclipse.ui.workbench" pref after))))
+  (let [font-registry (-> context
+                        (m/context-key org.eclipse.ui.IWorkbench)
+                        .getThemeManager
+                        .getCurrentTheme
+                        .getFontRegistry)]
+  (doseq [pref-key font-prefs-keys]
+    (when-let [font-data (first (.getFontData font-registry pref-key))] ;; FIXME what if font-data count > 1 ? What does that mean?
+      (let [new-font-data (org.eclipse.swt.graphics.FontData. (str font-data))
+            fds (str (doto new-font-data
+                       (.setHeight (float (+ increment (.getHeight new-font-data)))))
+                  ";")] ;; FIXME is this ";" really mandatory ?
+        (e/preference! "org.eclipse.ui.workbench" pref-key fds))))))
 
 (defn zoom-in-hdl [context] (zoomer font-increment-step context))
 (defn zoom-out-hdl [context] (zoomer (- font-increment-step) context))
@@ -62,5 +51,3 @@
 (defcommand zoom-out "Unzoom: decrease the fonts size (editors, repls, etc.)")
 (defhandler zoom-out zoom-out-hdl)
 (defkeybinding zoom-out "Alt+U U")
-
-
